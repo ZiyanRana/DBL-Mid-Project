8b801fe18a2cb98c14abcba3692fc4d4fbf0072d
ZiyanRana <mziyanrana@gmail.com>
2026-04-22 21:21:49 +0500

    Upload completed module: Assign Projects Module

---
+++ MidDb26_2024CS49/GroupProjectsForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MidDb26_2024CS49
+{
+    partial class GroupProjectsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbGroups = new System.Windows.Forms.ComboBox();
+            this.cmbProjects = new System.Windows.Forms.ComboBox();
+            this.assignBtn = new System.Windows.Forms.Button();
+            this.displayAssignments = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.displayAssignments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbGroups
+            // 
+            this.cmbGroups.FormattingEnabled = true;
+            this.cmbGroups.Location = new System.Drawing.Point(166, 46);
+            this.cmbGroups.Name = "cmbGroups";
+            this.cmbGroups.Size = new System.Drawing.Size(158, 28);
+            this.cmbGroups.TabIndex = 0;
+            // 
+            // cmbProjects
+            // 
+            this.cmbProjects.FormattingEnabled = true;
+            this.cmbProjects.Location = new System.Drawing.Point(166, 98);
+            this.cmbProjects.Name = "cmbProjects";
+            this.cmbProjects.Size = new System.Drawing.Size(158, 28);
+            this.cmbProjects.TabIndex = 1;
+            // 
+            // assignBtn
+            // 
+            this.assignBtn.Location = new System.Drawing.Point(166, 161);
+            this.assignBtn.Name = "assignBtn";
+            this.assignBtn.Size = new System.Drawing.Size(158, 40);
+            this.assignBtn.TabIndex = 2;
+            this.assignBtn.Text = "Assign Project";
+            this.assignBtn.UseVisualStyleBackColor = true;
+            this.assignBtn.Click += new System.EventHandler(this.assignBtn_Click);
+            // 
+            // displayAssignments
+            // 
+            this.displayAssignments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.displayAssignments.Location = new System.Drawing.Point(166, 248);
+            this.displayAssignments.Name = "displayAssignments";
+            this.displayAssignments.ReadOnly = true;
+            this.displayAssignments.RowHeadersWidth = 62;
+            this.displayAssignments.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.displayAssignments.RowTemplate.Height = 28;
+            this.displayAssignments.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.displayAssignments.Size = new System.Drawing.Size(449, 150);
+            this.displayAssignments.TabIndex = 3;
+            // 
+            // GroupProjectsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.displayAssignments);
+            this.Controls.Add(this.assignBtn);
+            this.Controls.Add(this.cmbProjects);
+            this.Controls.Add(this.cmbGroups);
+            this.Name = "GroupProjectsForm";
+            this.Text = "Assign Projects to Groups";
+            this.Load += new System.EventHandler(this.GroupProjectsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.displayAssignments)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbGroups;
+        private System.Windows.Forms.ComboBox cmbProjects;
+        private System.Windows.Forms.Button assignBtn;
+        private System.Windows.Forms.DataGridView displayAssignments;
+    }
+}
---
+++ MidDb26_2024CS49/GroupProjectsForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidDb26_2024CS49
+{
+    public partial class GroupProjectsForm : Form
+    {
+        DBHelper db = new DBHelper();
+        public GroupProjectsForm()
+        {
+            InitializeComponent();
+        }
+
+        void LoadGroups()
+        {
+            string query = @"SELECT Id, CONCAT('Group ', Id) AS GroupName FROM `group`";
+
+            DataTable dt = db.GetData(query);
+
+            cmbGroups.DataSource = dt;
+            cmbGroups.DisplayMember = "GroupName";
+            cmbGroups.ValueMember = "Id";
+        }
+
+        void LoadProjects()
+        {
+            string query = @"SELECT Id, Title FROM project";
+
+            DataTable dt = db.GetData(query);
+
+            cmbProjects.DataSource = dt;
+            cmbProjects.DisplayMember = "Title";
+            cmbProjects.ValueMember = "Id";
+        }
+
+        void LoadAssignments()
+        {
+            string query = @"
+                            SELECT gp.GroupId, p.Title, gp.AssignmentDate
+                            FROM groupproject gp
+                            JOIN project p ON gp.ProjectId = p.Id";
+
+            displayAssignments.DataSource = db.GetData(query);
+        }
+
+        private void GroupProjectsForm_Load(object sender, EventArgs e)
+        {
+            LoadGroups();
+            LoadProjects();
+            LoadAssignments();
+        }
+
+        private void assignBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int groupId = Convert.ToInt32(cmbGroups.SelectedValue);
+                int projectId = Convert.ToInt32(cmbProjects.SelectedValue);
+
+                string checkQuery = $@"
+                                    SELECT *
+                                    FROM groupproject
+                                    WHERE GroupId = {groupId}";
+
+                if (db.GetData(checkQuery).Rows.Count > 0)
+                {
+                    MessageBox.Show("This group already has a project assigned!");
+                    return;
+                }
+
+                string query = $@"
+                                INSERT INTO groupproject (ProjectId, GroupId, AssignmentDate)
+                                VALUES
+                                ({projectId}, {groupId}, NOW())";
+
+                db.ExecuteQuery(query);
+                MessageBox.Show("Project Assigned!");
+                LoadAssignments();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
