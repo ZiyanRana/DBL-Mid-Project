bca96b4988ccc3c96cda2bd00bd0f3e7ae4bdd49
ZiyanRana <mziyanrana@gmail.com>
2026-04-23 00:13:36 +0500

    Upload completed module: Project Advisors Module

---
+++ MidDb26_2024CS49/ProjectAdvisorsForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace MidDb26_2024CS49
+{
+    partial class ProjectAdvisorsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbAdvisors = new System.Windows.Forms.ComboBox();
+            this.cmbRoles = new System.Windows.Forms.ComboBox();
+            this.assignAdvisorBtn = new System.Windows.Forms.Button();
+            this.displayAssignments = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbProjects = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.displayAssignments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbAdvisors
+            // 
+            this.cmbAdvisors.FormattingEnabled = true;
+            this.cmbAdvisors.Location = new System.Drawing.Point(300, 64);
+            this.cmbAdvisors.Name = "cmbAdvisors";
+            this.cmbAdvisors.Size = new System.Drawing.Size(121, 28);
+            this.cmbAdvisors.TabIndex = 0;
+            // 
+            // cmbRoles
+            // 
+            this.cmbRoles.FormattingEnabled = true;
+            this.cmbRoles.Location = new System.Drawing.Point(439, 64);
+            this.cmbRoles.Name = "cmbRoles";
+            this.cmbRoles.Size = new System.Drawing.Size(121, 28);
+            this.cmbRoles.TabIndex = 1;
+            // 
+            // assignAdvisorBtn
+            // 
+            this.assignAdvisorBtn.Location = new System.Drawing.Point(286, 153);
+            this.assignAdvisorBtn.Name = "assignAdvisorBtn";
+            this.assignAdvisorBtn.Size = new System.Drawing.Size(149, 39);
+            this.assignAdvisorBtn.TabIndex = 2;
+            this.assignAdvisorBtn.Text = "Assign Advisor";
+            this.assignAdvisorBtn.UseVisualStyleBackColor = true;
+            this.assignAdvisorBtn.Click += new System.EventHandler(this.assignAdvisorBtn_Click);
+            // 
+            // displayAssignments
+            // 
+            this.displayAssignments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.displayAssignments.Location = new System.Drawing.Point(161, 247);
+            this.displayAssignments.Name = "displayAssignments";
+            this.displayAssignments.ReadOnly = true;
+            this.displayAssignments.RowHeadersWidth = 62;
+            this.displayAssignments.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.displayAssignments.RowTemplate.Height = 28;
+            this.displayAssignments.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.displayAssignments.Size = new System.Drawing.Size(399, 150);
+            this.displayAssignments.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Advisor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(439, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select Role";
+            // 
+            // cmbProjects
+            // 
+            this.cmbProjects.FormattingEnabled = true;
+            this.cmbProjects.Location = new System.Drawing.Point(161, 64);
+            this.cmbProjects.Name = "cmbProjects";
+            this.cmbProjects.Size = new System.Drawing.Size(121, 28);
+            this.cmbProjects.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Select Project";
+            // 
+            // ProjectAdvisorsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbProjects);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.displayAssignments);
+            this.Controls.Add(this.assignAdvisorBtn);
+            this.Controls.Add(this.cmbRoles);
+            this.Controls.Add(this.cmbAdvisors);
+            this.Name = "ProjectAdvisorsForm";
+            this.Text = "Assign Advisors to Projects";
+            this.Load += new System.EventHandler(this.ProjectAdvisorsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.displayAssignments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbAdvisors;
+        private System.Windows.Forms.ComboBox cmbRoles;
+        private System.Windows.Forms.Button assignAdvisorBtn;
+        private System.Windows.Forms.DataGridView displayAssignments;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbProjects;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ MidDb26_2024CS49/ProjectAdvisorsForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidDb26_2024CS49
+{
+    public partial class ProjectAdvisorsForm : Form
+    {
+        DBHelper db = new DBHelper();
+        public ProjectAdvisorsForm()
+        {
+            InitializeComponent();
+        }
+
+        void LoadProjects()
+        {
+            string query = "SELECT Id, Title FROM project";
+
+            DataTable dt = db.GetData(query);
+
+            cmbProjects.DataSource = dt;
+            cmbProjects.DisplayMember = "Title";
+            cmbProjects.ValueMember = "Id";
+        }
+
+        void LoadAdvisors()
+        {
+            string query = @"
+                            SELECT a.Id, CONCAT(p.FirstName, ' ', p.LastName) AS AdvisorName
+                            FROM advisor a
+                            JOIN person p ON a.Id = p.Id";
+
+            DataTable dt = db.GetData(query);
+
+            cmbAdvisors.DataSource = dt;
+            cmbAdvisors.DisplayMember = "AdvisorName";
+            cmbAdvisors.ValueMember = "Id";
+        }
+
+        void LoadRoles()
+        {
+            string query = @"
+                            SELECT Id, Value
+                            FROM lookup
+                            WHERE Category = 'ADVISOR_ROLE'";
+
+            DataTable dt = db.GetData(query);
+
+            cmbRoles.DataSource = dt;
+            cmbRoles.DisplayMember = "Value";
+            cmbRoles.ValueMember = "Id";
+        }
+
+        void LoadAssignments()
+        {
+            string query = @"
+                            SELECT 
+                                p.Title AS Project, 
+                                CONCAT(pe.FirstName, ' ', pe.LastName) AS Advisor,
+                                l.Value AS Role, 
+                                pa.AssignmentDate
+
+                            FROM projectadvisor pa
+
+                            JOIN project p ON pa.ProjectId = p.Id
+                            JOIN advisor a ON pa.AdvisorId = a.Id
+                            JOIN person pe ON a.Id = pe.Id
+                            JOIN lookup l ON pa.AdvisorRole = l.Id";
+      
+            displayAssignments.DataSource = db.GetData(query);
+        }
+        private void ProjectAdvisorsForm_Load(object sender, EventArgs e)
+        {
+            LoadProjects();
+            LoadAdvisors();
+            LoadRoles();
+            LoadAssignments();
+        }
+
+        private void assignAdvisorBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int projectId = Convert.ToInt32(cmbProjects.SelectedValue);
+                int advisorId = Convert.ToInt32(cmbAdvisors.SelectedValue);
+                int roleId = Convert.ToInt32(cmbRoles.SelectedValue);
+
+                string checkQuery = $@"
+                                    SELECT *
+                                    FROM projectadvisor
+                                    WHERE ProjectId = {projectId}
+                                    AND AdvisorRole = {roleId}";
+
+                if (db.GetData(checkQuery).Rows.Count > 0)
+                {
+                    MessageBox.Show("This role is already assigned for this project!");
+                    return;
+                }
+
+                string query = $@"
+                                INSERT INTO projectadvisor(AdvisorId, ProjectId, AdvisorRole, AssignmentDate)
+                                VALUES
+                                ({advisorId}, {projectId}, {roleId}, NOW())";
+
+                db.ExecuteQuery(query);
+                MessageBox.Show("Advisor Assigned!");
+                LoadAssignments();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
