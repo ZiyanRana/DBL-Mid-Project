e3111d9689a33afea63b1c9df067ab080bd17055
ZiyanRana <mziyanrana@gmail.com>
2026-04-24 18:06:24 +0500

    Upload completed module: Reports

---
+++ MidDb26_2024CS49/ReportsForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace MidDb26_2024CS49
+{
+    partial class ReportsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.projectReportBtn = new System.Windows.Forms.Button();
+            this.marksheetReportBtn = new System.Windows.Forms.Button();
+            this.displayReports = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.displayReports)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // projectReportBtn
+            // 
+            this.projectReportBtn.Location = new System.Drawing.Point(163, 107);
+            this.projectReportBtn.Name = "projectReportBtn";
+            this.projectReportBtn.Size = new System.Drawing.Size(193, 47);
+            this.projectReportBtn.TabIndex = 0;
+            this.projectReportBtn.Text = "Project Report";
+            this.projectReportBtn.UseVisualStyleBackColor = true;
+            this.projectReportBtn.Click += new System.EventHandler(this.projectReportBtn_Click);
+            // 
+            // marksheetReportBtn
+            // 
+            this.marksheetReportBtn.Location = new System.Drawing.Point(443, 107);
+            this.marksheetReportBtn.Name = "marksheetReportBtn";
+            this.marksheetReportBtn.Size = new System.Drawing.Size(193, 47);
+            this.marksheetReportBtn.TabIndex = 1;
+            this.marksheetReportBtn.Text = "Marksheet Report";
+            this.marksheetReportBtn.UseVisualStyleBackColor = true;
+            this.marksheetReportBtn.Click += new System.EventHandler(this.marksheetReportBtn_Click);
+            // 
+            // displayReports
+            // 
+            this.displayReports.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.displayReports.Location = new System.Drawing.Point(163, 225);
+            this.displayReports.Name = "displayReports";
+            this.displayReports.RowHeadersWidth = 62;
+            this.displayReports.RowTemplate.Height = 28;
+            this.displayReports.Size = new System.Drawing.Size(473, 150);
+            this.displayReports.TabIndex = 2;
+            // 
+            // ReportsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.displayReports);
+            this.Controls.Add(this.marksheetReportBtn);
+            this.Controls.Add(this.projectReportBtn);
+            this.Name = "ReportsForm";
+            this.Text = "View Reports";
+            ((System.ComponentModel.ISupportInitialize)(this.displayReports)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button projectReportBtn;
+        private System.Windows.Forms.Button marksheetReportBtn;
+        private System.Windows.Forms.DataGridView displayReports;
+    }
+}
---
+++ MidDb26_2024CS49/ReportsForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidDb26_2024CS49
+{
+    public partial class ReportsForm : Form
+    {
+        DBHelper db = new DBHelper();
+        public ReportsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void projectReportBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = @"
+                                SELECT
+                                    p.Title AS Project, 
+                                    gp.GroupId, 
+                                    CONCAT(pe.FirstName, ' ', pe.LastName) AS Advisor,
+                                    l.Value AS AdvisorRole
+                                FROM groupproject gp
+                                JOIN project p ON gp.ProjectId = p.Id
+                                JOIN projectadvisor pa ON p.Id = pa.ProjectId
+                                JOIN advisor a ON pa.AdvisorId = a.Id
+                                JOIN person pe ON a.Id = pe.Id
+                                JOIN lookup l ON pa.AdvisorRole = l.Id
+                                ORDER BY p.Title;";
+
+                displayReports.DataSource = db.GetData(query);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void marksheetReportBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = @"
+                                SELECT
+                                    ge.GroupId,
+                                    e.Name AS Evaluation,
+                                    ge.ObtainedMarks,
+                                    e.TotalMarks,
+                                    (
+                                        ge.ObtainedMarks * 100/e.TotalMarks) AS Percentage
+                                    FROM groupevaluation ge
+                                    JOIN evaluation e ON ge.EvaluationId = e.Id;";
+
+                displayReports.DataSource = db.GetData(query);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
